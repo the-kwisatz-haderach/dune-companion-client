f46d0983689cc04b3da9b65ffe004091de956706
Storchillarn <gurr_gurr@hotmail.com>
2021-02-21 20:00:38 +0100

    Further refactoring

--- hooks/useForm/schema/index.ts
+++ hooks/useForm/schema/index.ts
@@ -1,3 +1,4 @@
 import schemaCreator from './createFormFields'
+export type { FieldTypeSchema, FormSchema } from './types'
 
 export default schemaCreator
--- hooks/useForm/useForm.ts
+++ hooks/useForm/useForm.ts
@@ -1,9 +1,8 @@
 import { useReducer, useCallback } from 'react'
-import { FieldTypeSchema, FormSchema } from './schema/types'
-import { formReducer, initFormState } from './reducer'
+import { FieldTypeSchema, FormSchema } from './schema'
+import { formReducer, initFormState, FormReducer } from './reducer'
 import { createSubmitHandler, createValueUpdater } from './helpers'
 import { SubmitHandler, UseFormProps } from './types'
-import { FormReducer } from './reducer/types'
 
 const useForm = <T extends FieldTypeSchema<any>>(
   formSchema: FormSchema<T> | T,
